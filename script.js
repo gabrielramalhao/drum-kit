4d2ff41e3a147bd7f41169c2f65bd219d038bef2
Gabriel Ramalhao <gabriel123.ramalhao@gmail.com>
2025-11-19 07:12:17 -0300

    feat: css animation added

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 window.addEventListener("keydown", (e) => {
   const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const key = document.querySelector(`div[data-key="${e.code}"]`);
+  key.classList.add("key-pressed");
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
+  setTimeout(() => {
+    key.classList.remove("key-pressed");
+  }, 1000);
 });
