4e5b6859ae060c62c43b485ecf095ac6d725b811
Gabriel Ramalhao <gabriel123.ramalhao@gmail.com>
2025-11-18 21:48:04 -0300

    feat: audio added

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("keydown", (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+});
